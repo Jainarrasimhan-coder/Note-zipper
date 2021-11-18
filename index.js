2dfbb420265aaba66a12802e29a86273b4ac6d5c
unknown <you@example.com>
2021-11-18 22:17:41 +0530

    make it better

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const connectDB = require("./config/db")
 const userRouter = require('./routes/userRourter');
 const { notFound, errorHandler } = require('./middleware/errMiddleware');
 const noteRouter = require("./routes/noteRouter");
-const path = require("path")
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+
 const app = express()
 dotenv.config();
 connectDB();
@@ -22,14 +24,15 @@ app.get('/', (req, res) => {
 //     const note = notes.find((n) => n._id === req.params.id)
 //     res.send(note)
 // })
+
 // --------------------------deployment------------------------------
 const _dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(_dirname, "/client/my-notes/build")));
+    app.use(express.static(path.join(_dirname, "/client/build")));
 
     app.get("*", (req, res) =>
-        res.sendFile(path.resolve(_dirname, "client", "my-notes", "build", "index.html"))
+        res.sendFile(path.resolve(_dirname, "client", "build", "index.html"))
     );
 } else {
     app.get("/", (req, res) => {
@@ -45,6 +48,6 @@ app.use(notFound)
 app.use(errorHandler)
 
 
-app.listen(5000, (req, res) => {
+app.listen(PORT, (req, res) => {
     console.log("app is running in port 5000")
 })
